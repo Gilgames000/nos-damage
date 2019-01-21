e8a278202ea59abcb48e77c7ddc2c8df88e3733f
Gilgames <andrea.audenino@protonmail.com>
2019-01-21 23:50:35 +0100

    No need to distinguish b/w attacker and defender

--- util/persistence.py
+++ util/persistence.py
@@ -4,20 +4,11 @@ import logging
 from datastructs import Entity
 
 
-def save_attacker(attacker, filename):
-    save_dataclass(attacker.to_dict(), filename)
+def save_entity(entity, filename):
+    save_dataclass(entity.to_dict(), filename)
 
 
-def save_defender(defender, filename):
-    save_dataclass(defender.to_dict(), filename)
-
-
-def load_attacker(filename):
-    d = load_dataclass(filename)
-    return Entity.from_dict(d)
-
-
-def load_defender(filename):
+def load_entity(filename):
     d = load_dataclass(filename)
     return Entity.from_dict(d)
 
