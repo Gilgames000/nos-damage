4e5ec362947c684e28fa4ac13b479f42255befff
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2019-01-22 18:58:59 +0100

    Add is_mob listeners

--- gui.py
+++ gui.py
@@ -30,9 +30,18 @@ def setup_btn_listeners():
 def display_editor_entity(index):
     entity = entities[index]
     ui.le_name.setText(entity.name)
-    ui.sb_level.setValue(entity.level)
     ui.dropdown_element.setCurrentIndex(entity.type)
-    print(entity.type, Element(entity.type))
+    ui.sb_level.setValue(entity.level)
+    ui.sb_fairy.setValue(entity.fairy)
+    ui.cb_is_mage.setChecked(entity.is_mage)
+    ui.cb_is_mob.setChecked(entity.is_mob)
+    if entity.is_mob:
+        ui.dropdown_mob_type.setEnabled(True)
+        ui.dropdown_mob_type.setCurrentIndex(entity.mob_type)
+    else:
+        ui.dropdown_mob_type.setEnabled(False)
+        ui.dropdown_mob_type.setCurrentIndex(MobType.NONE)
+    ui.sb_atk_base.setValue(entity.atk_base)
 
 
 if __name__ == "__main__":
@@ -48,8 +57,14 @@ if __name__ == "__main__":
     ui.dropdown_element.addItems([element.name.lower() for element in Element])
     ui.dropdown_mob_type.addItems([mtype.name.lower() for mtype in MobType])
 
+    ui.cb_is_mob.clicked.connect(
+        lambda checked: ui.dropdown_mob_type.setEnabled(checked)
+    )
+
     ui.dropdown_entity.currentIndexChanged.connect(display_editor_entity)
 
+    ui.stacked_editor.setCurrentIndex(0)
+    setup_btn_listeners()
     display_editor_entity(0)
 
     for i in entities:
