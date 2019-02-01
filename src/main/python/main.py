de0d562804c64467724b7dcea1823a9abcbd36d0
Gilgames_ <andrea.audenino@protonmail.com>
2019-02-02 00:13:01 +0100

    Add VersionCheckerService and dialog

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,6 +1,7 @@
 import sys
 
 from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMessageBox
 from fbs_runtime.application_context import ApplicationContext
 
 import util.persistence as ps
@@ -8,23 +9,49 @@ from controllers.calculator import CalculatorController
 from controllers.editor import EditorController
 from gui import Ui_MainWindow
 from util.observables import ObservableList
+from util.versiontools import VersionCheckerService
 
 
 class AppContext(ApplicationContext):  # 1. Subclass ApplicationContext
+    def show_update_dialog(self):
+        title = "NosDamage update"
+        message = (
+            "A new version is available. "
+            "Do you want to open the download page?"
+        )
+        buttons = QMessageBox.Yes | QMessageBox.No
+
+        reply = QMessageBox.question(self.ui, title, message, buttons)
+
+        if reply == QMessageBox.Yes:
+            import webbrowser
+            url = "https://github.com/Gilgames000/nos-damage/releases"
+            webbrowser.open(url)
+
     def run(self):  # 2. Implement run()
         window = QMainWindow()
-        ui = Ui_MainWindow()
+        self.ui = Ui_MainWindow()
 
-        ui.setupUi(window)
+        self.ui.setupUi(window)
         version = self.build_settings['version']
         window.setWindowTitle("NosDamage v" + version)
         window.resize(250, 150)
         window.show()
 
+        version_checker = VersionCheckerService(version)
+        version_checker.needUpdate.connect(
+            lambda need_update: (
+                self.show_update_dialog()
+                if need_update
+                else None
+            )
+        )
+        version_checker.start()
+
         entities = ObservableList(ps.load_entities())
 
-        self.editor_controller = EditorController(ui, entities)
-        self.calculator_controller = CalculatorController(ui, entities)
+        self.editor_controller = EditorController(self.ui, entities)
+        self.calculator_controller = CalculatorController(self.ui, entities)
 
         entities.add_observer(self.calculator_controller)
 
